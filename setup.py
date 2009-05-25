0e7e8713b3577030153c8ac7df4e9c172f60117b
Andrew Godwin <andrew@aeracode.org>
2009-05-25 16:42:59 +0000

    Cleanup for insertion into PyPI

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 from graphication import release
 
 setup(
+	zip_safe = False,
 	name = release.product,
 	version = release.version,
 	packages = find_packages(),
+	package_data = {'graphication': ['*.css']},
 	description = "A Cairo-based graphing library",
 	author = "Andrew Godwin",
 	author_email = "graphication@aeracode.org",
